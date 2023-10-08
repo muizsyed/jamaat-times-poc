ec1df1fd8cbca151b2256936b7578598f52845d5
Muiz Syed <muizsyed@gmail.com>
2023-10-08 21:37:23 +0100

    Load masjid data on startup

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
 )
 
+func daysIn(m time.Month, year int) int {
+	// This is equivalent to time.daysIn(m, year).
+	return time.Date(year, m+1, 0, 0, 0, 0, 0, time.UTC).Day()
+}
+
+var masjids []*Masjid
+var masjidDay map[string]*[]Day = map[string]*[]Day{}
+
 func main() {
+
+	loadData()
+
 	engine := html.New("./views", ".html")
 	engine.Reload(true)
 
@@ -18,24 +34,107 @@ func main() {
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
+
+		day := time.Now().Day()
+
+		masjidDays := make([]MasjidDay, len(masjids))
+
+		for i := 0; i < len(masjids); i++ {
+			currentMasjid := masjids[i]
+			days := *masjidDay[currentMasjid.Slug]
+			masjidDays[i] = MasjidDay{
+				Masjid: *currentMasjid,
+				Day:    days[day],
+			}
+		}
+
 		return c.Render("index", fiber.Map{
-			"Title":    "Hello, World!",
-			"Revision": appRevision,
+			"Title":      "Hello, World!",
+			"Revision":   appRevision,
+			"Masjids":    masjids,
+			"MasjidDays": masjidDays,
 		}, "layouts/main")
 	})
 
 	app.Listen(":8081")
 }
 
+func loadData() {
+	path, _ := os.Getwd()
+	fmt.Fprintln(os.Stdout, "Path is:", path, ", and month is", strings.ToLower(time.Now().Month().String()), int(time.Now().Month()))
+	monthFile := fmt.Sprintf("%d.json", int(time.Now().Month()))
+	fileInfos, _ := os.ReadDir(filepath.Join(path, "data"))
+
+	for _, masjidDirectory := range fileInfos {
+		fmt.Printf("Masjid: %s\n", masjidDirectory.Name())
+		metadataPath := filepath.Join(path, "data", masjidDirectory.Name(), "metadata.json")
+		timetablePath := filepath.Join(path, "data", masjidDirectory.Name(), monthFile)
+
+		masjid := loadMasjidMetadata(metadataPath)
+		//masjidda, _ := json.Marshal(masjid)
+		//fmt.Println(string(masjidda))
+
+		days := loadMasjidTimetable(timetablePath)
+
+		masjids = append(masjids, masjid)
+		masjidDay[masjid.Slug] = days
+
+	}
+
+}
+
+func loadMasjidMetadata(path string) *Masjid {
+	data, err := os.ReadFile(path)
+
+	fmt.Fprintln(os.Stdout, "Loading metadata", path)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	masjid := &Masjid{}
+
+	json.Unmarshal(data, masjid)
+
+	return masjid
+}
+
+func loadMasjidTimetable(path string) *[]Day {
+	data, err := os.ReadFile(path)
+
+	fmt.Fprintln(os.Stdout, "Loading timetable", path)
+
+	if err != nil {
+		fmt.Println(err)
+		//return nil
+	}
+
+	days := make([]Day, daysIn(time.Now().Month(), time.Now().Year()))
+	err = json.Unmarshal(data, &days)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return &days
+}
+
 type Masjid struct {
-	Name     string
+	Name     string `json:"name"`
+	Slug     string `json:"slug"`
 	City     string
 	Postcode string
 }
 
+type MasjidDay struct {
+	Masjid Masjid
+	Day    Day
+}
+
 type Prayer struct {
-	Start  string
-	Jamaat string
+	Start  string `json:"start"`
+	Jamaat string `json:"jamaat"`
 }
 
 type Month struct {
@@ -44,9 +143,10 @@ type Month struct {
 }
 
 type Day struct {
-	Fajr    Prayer
-	Zuhr    Prayer
-	Asr     Prayer
-	Maghrib Prayer
-	Esha    Prayer
+	Date    string `json:"date"`
+	Fajr    Prayer `json:"fajr"`
+	Zuhr    Prayer `json:"zuhr"`
+	Asr     Prayer `json:"asr"`
+	Maghrib Prayer `json:"maghrib"`
+	Esha    Prayer `json:"esha"`
 }
